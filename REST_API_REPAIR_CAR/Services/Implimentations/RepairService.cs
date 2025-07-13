eafa1d395b86c3a3f169f197b986214715f84707
zoxoxx <spaorto740@gmail.com>
2025-07-13 21:52:58 +0300

    Save last state realisation service "Repair service".

---
+++ REST_API_REPAIR_CAR/Services/Implimentations/RepairService.cs
@@ -0,0 +1,25 @@
+﻿using REST_API_REPAIR_CAR.Models;
+using REST_API_REPAIR_CAR.Repositories.Interfaces;
+using REST_API_REPAIR_CAR.Services.Interfaces;
+
+namespace REST_API_REPAIR_CAR.Services.Implimentations
+{
+    public class RepairService : IRepairService
+    {
+        private IBaseRepository<Document> Documents { get; set; }
+        private IBaseRepository<Car> Cars { get; set; }
+        private IBaseRepository<Worker> Workers { get; set; }
+
+        public void Work()
+        {
+            var rand = new Random();
+            var carId = Guid.NewGuid();
+            var WorkerId = Guid.NewGuid();
+
+            Cars.Create(new Car
+            {
+
+            });
+        }
+    }
+}
---
+++ REST_API_REPAIR_CAR/Services/Interfaces/IRepairService.cs
@@ -0,0 +1,7 @@
+﻿namespace REST_API_REPAIR_CAR.Services.Interfaces
+{
+    public interface IRepairService
+    {
+        public void Work();
+    }
+}
